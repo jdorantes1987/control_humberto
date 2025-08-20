30e5ed2ab562b75fdc4c6305df6cb83b10d2b491
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-08-20 11:31:54 -0400

    Initial commit

---
+++ Home.py
@@ -0,0 +1,58 @@
+
+from matplotlib import pyplot as plt
+import streamlit as st
+
+from data_manage import DataManageSingleton
+from data_sheets import ManageSheets
+
+# Inicializa el gestor de hojas con los parámetros necesarios
+SPREADSHEET_ID = st.secrets.google_sheets.FILE_ID
+FILE_NAME = st.secrets.google_sheets.FILE_NAME
+CREDENTIALS_DICT = dict(st.secrets.google_service_account)
+
+st.set_page_config(page_title="Inicio", page_icon="🏠", layout="centered")
+
+st.markdown("""
+<div style='text-align: center;'>
+    <h1 style='color: #2c2c2c;'>Bienvenido al Panel de Control <span style='color: #336699;'>Humberto</span></h1>
+    <p style='font-size: 1.2em;'>Gestiona y visualiza el consolidado de montos de manera sencilla y rápida.</p>
+</div>
+""", unsafe_allow_html=True)
+
+st.write("")
+
+for key, default in [
+    ("stage", 0),
+    ("pivot_consolidado", None),
+    ("manager_sheets", None),
+    ("pv_consolidado", None),
+]:
+    if key not in st.session_state:
+        st.session_state[key] = default
+
+def set_state(i):
+    st.session_state.stage = i
+
+if st.session_state.stage == 0:
+    # Inicializar el estado de la sesión
+    # Obtiene la instancia del gestor de datos
+    with st.spinner("Actualizando datos..."):
+            st.session_state.manager_sheets = ManageSheets(
+                file_sheet_name=FILE_NAME,
+                spreadsheet_id=SPREADSHEET_ID,
+                credentials_file=CREDENTIALS_DICT,
+            )
+    data_manager = DataManageSingleton.get_instance(st.session_state.manager_sheets)
+    st.session_state.pivot_consolidado = data_manager.get_pivot_consolidado()
+    set_state(1)
+
+if st.session_state.stage == 1:
+    format_dict = {
+        "Productos": "{:,.2f}",
+        "Fletes": "{:,.2f}",
+        "Saldo": "{:,.2f}",
+    }  # ejemplo {'sum':'${0:,.0f}', 'date': '{:%m-%Y}', 'pct_of_total': '{:.2%}'}
+    cmap = plt.colormaps["RdYlGn"]
+    st.dataframe(st.session_state.pivot_consolidado.style.format(format_dict).background_gradient(subset=["Saldo"], cmap=cmap,), hide_index=True) # Aplicar formato y color a la columna "Saldo"
+    st.button(label="Actualizar", on_click=set_state, args=(0,), icon="🔄")
+        
--- data_manage.py
+++ data_manage.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame, to_datetime, to_numeric
+from pandas import DataFrame, concat, to_numeric, to_datetime
+from numpy import where
 
 from data_control import DataControl
 
@@ -33,6 +34,61 @@ class DataManage:
         Obtiene los datos de fletes.
         """
         return self.oDataControl.get_fletes()
+    
+    def get_consolidado(self) -> DataFrame:
+        """
+        Obtiene el consolidado de productos y fletes.
+        """
+        productos = self.get_productos()
+        fletes = self.get_fletes()
+        consolidado = concat([productos, fletes], axis=0)
+        cols_montos = [
+            "monto",
+        ]  # Lista de columnas que contienen montos
+        # Eliminar separadores de miles y reemplazar coma decimal por punto
+        for col in cols_montos:
+            consolidado[col] = (
+                consolidado[col]
+                .str.replace(".", "", regex=False)  # Remove thousand separator
+                .str.replace(",", ".", regex=False)  # Replace decimal comma with dot
+            )
+        try:
+            # Convertir a float, forzando errores a NaN
+            consolidado[cols_montos] = consolidado[cols_montos].apply(
+                to_numeric, errors="raise"  # Convertir a float errores a NaN
+            )
+        except Exception as e:
+            print(f"Error al convertir columnas a numéricas: {e}")
+            consolidado = DataFrame()  # Si hay un error, devuelve un DataFrame vacío
+        consolidado["monto"] = where(
+            consolidado["tipo"]=="Fletes", -consolidado["monto"], consolidado["monto"]
+        )
+        consolidado['fecha'] = to_datetime(consolidado['fecha'], format='%d/%m/%Y')
+
+
+        return consolidado
+    
+    def get_pivot_consolidado(self) -> DataFrame:
+        """
+        Obtiene un pivot consolidado de productos y fletes.
+        """
+        consolidado = self.get_consolidado()
+        if consolidado.empty:
+            return DataFrame()
+
+        consolidado["periodo"] = consolidado["fecha"].dt.to_period("M")
+        pivot_consolidado = consolidado.pivot_table(
+            index=["periodo"],
+            columns=["tipo"],
+            values="monto",
+            aggfunc="sum",
+            fill_value=0,
+            sort=False,
+        ).reset_index()
+        pivot_consolidado.sort_values(by="periodo", inplace=True)
+        pivot_consolidado = pivot_consolidado.groupby("periodo").sum().reset_index()
+        pivot_consolidado["Saldo"] = (pivot_consolidado["Productos"] + pivot_consolidado["Fletes"]).cumsum()
+        return pivot_consolidado
 
 
 if __name__ == "__main__":
@@ -49,9 +105,5 @@ if __name__ == "__main__":
     # Obtiene la instancia del gestor de datos
     data_manager = DataManageSingleton.get_instance(manager_sheets)
 
-    # Obtiene los productos y fletes
-    productos = data_manager.get_productos()
-    fletes = data_manager.get_fletes()
-
-    print(productos)
-    print(fletes)
+    consolidado = data_manager.get_pivot_consolidado()
+    print(consolidado)
