c41f95e236e2a9db90fe8ca05c0cd65c40383c6d
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-08-21 15:39:08 -0400

    Orden columna "periodo" descendente .

--- data_manage.py
+++ data_manage.py
@@ -84,7 +84,7 @@ class DataManage:
             fill_value=0,
             sort=False,
         ).reset_index()
-        pivot_consolidado.sort_values(by="periodo", inplace=True)
+        pivot_consolidado.sort_values(by="periodo", ascending=False, inplace=True)
         pivot_consolidado = pivot_consolidado.groupby("periodo").sum().reset_index()
         pivot_consolidado["Neto"] = (
             pivot_consolidado["Productos"] + pivot_consolidado["Fletes"]
